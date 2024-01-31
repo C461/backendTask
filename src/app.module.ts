64414ba3035c631f4d9217c2b20045c9524dfd4a
C461 <kozefra@gmai.com>
2024-01-31 14:39:36 -0500

    Actualizando repo2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/taskapp'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://kozefra:<password>@contacts-porfolio.sloal1k.mongodb.net/?retryWrites=true&w=majority`,
+    ),
     TasksModule,
   ],
 })
