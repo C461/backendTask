df136e572eea22bb3ca4be9a636fd5a45bfa5ede
C461 <kozefra@gmai.com>
2024-02-02 12:10:33 -0500

    updating main

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.port || 3000;
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.port || 4000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
